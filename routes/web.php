d97924584bb4becc224c8931d6ac25697c2af351
NandiMR <150249568+NandiMR@users.noreply.github.com>
2023-11-29 18:53:25 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product; // Tambahkan use statement untuk model Product
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,26 +17,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () {
-   
-$data = [
-    'pageTitle' => 'Tentang Kami',
-    'content' => 'Ini adalah halaman tentang kami.'
-];
-return view('about', $data);
-});
-Route::get('/produk', function () {
-    $data = [
-        'produk1', 'produk2', 'produk3'
-    ];
 
-    return view('produk', ['data' => $data]);
+Route::get('/products', function () {
+    $products = App\Models\Product::all();
+    return view('products.index', ['products' => $products]);
 });
-Route::get('/user', function () {
-   
+Route::get('/about', function () {
     $data = [
         'pageTitle' => 'Tentang Kami',
         'content' => 'Ini adalah halaman tentang kami.'
     ];
+    return view('about', $data);
+});
+
+Route::get('/produk', function () {
+    $data = [
+        'produk1' => 'Nama Produk 1', // Tambahkan key untuk setiap produk
+        'produk2' => 'Nama Produk 2',
+        'produk3' => 'Nama Produk 3'
+    ];
+
+    return view('produk', ['data' => $data]);
+});
+
+Route::get('/user', function () {
+    $data = [
+        'pageTitle' => 'Halaman Pengguna',
+        'content' => 'Ini adalah halaman pengguna.'
+    ];
     return view('user', $data);
-    });
+});
+
+Route::get('/products', 'ProductController@index');
